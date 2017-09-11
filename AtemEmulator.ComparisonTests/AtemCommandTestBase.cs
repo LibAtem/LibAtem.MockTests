3c729ad1fd113c005fe248519e443802d850d1cf
Julian <git@julusian.co.uk>
2017-09-11 22:22:51 +0100

    Add sample comparison test against official atem sdk

---
+++ AtemEmulator.ComparisonTests/AtemCommandTestBase.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using BMDSwitcherAPI;
+using LibAtem.Commands;
+using LibAtem.Net;
+using Xunit;
+
+namespace AtemEmulator.ComparisonTests
+{
+    public abstract class AtemCommandTestBase : IDisposable
+    {
+        private readonly AtemClient _client;
+        private bool _clientConnected;
+
+
+        private readonly AutoResetEvent _disposeEvent;
+        private bool _isDisposing;
+
+        private readonly IBMDSwitcherDiscovery _switcherDiscovery;
+        protected readonly IBMDSwitcher _sdkSwitcher;
+
+        private readonly List<ICommand> _receivedCommands;
+
+        private bool _handshakeFinished;
+        private readonly AutoResetEvent _handshakeEvent;
+
+        protected AtemCommandTestBase()
+        {
+            var connectionEvent = new AutoResetEvent(false);
+            _disposeEvent = new AutoResetEvent(false);
+            _handshakeEvent = new AutoResetEvent(false);
+
+            _receivedCommands = new List<ICommand>();
+
+            _client = new AtemClient("127.0.0.1");
+            _client.OnConnection += s =>
+            {
+                if (_clientConnected)
+                    Assert.True(false, "LibAtem: Got unexpected reconnect");
+
+                _clientConnected = true;
+                connectionEvent.Set();
+            };
+            _client.OnDisconnect += s =>
+            {
+                if (!_isDisposing)
+                    Assert.True(false, "LibAtem: Got early termination");
+                
+                _clientConnected = false;
+                _disposeEvent.Set();
+            };
+            _client.OnReceive += (s, commands) =>
+            {
+                lock (_receivedCommands)
+                {
+                    if (_handshakeFinished || LogLibAtemHandshake)
+                        _receivedCommands.AddRange(commands);
+
+                    if (!_handshakeFinished && commands.Any(c => c is InitializationCompleteCommand))
+                    {
+                        _handshakeEvent.Set();
+                        _handshakeFinished = true;
+                    }
+                }
+            };
+
+            Assert.True(connectionEvent.WaitOne(TimeSpan.FromSeconds(3)), "LibAtem: Connection attempt timed out");
+
+            _switcherDiscovery = new CBMDSwitcherDiscovery();
+            Assert.NotNull(_switcherDiscovery);
+
+            _BMDSwitcherConnectToFailure failReason = 0;
+            try
+            {
+                _switcherDiscovery.ConnectTo("127.0.0.1", out _sdkSwitcher, out failReason);
+            }
+            catch (COMException)
+            {
+                throw new Exception($"SDK Connection failure: {failReason}");
+            }
+
+            _sdkSwitcher.AddCallback(new SwitcherConnectionMonitor()); // TODO - make this monitor work better!
+
+            WaitForHandshake();
+        }
+
+        protected virtual bool LogLibAtemHandshake => false;
+
+        private bool WaitForHandshake(bool errorOnTimeout=true)
+        {
+            bool res = _handshakeEvent.WaitOne(TimeSpan.FromSeconds(20));
+
+            if (errorOnTimeout)
+                Assert.True(res);
+
+            return res;
+        }
+
+        public void ClearReceivedCommands()
+        {
+            lock(_receivedCommands)
+                _receivedCommands.Clear();
+        }
+
+        public List<T> GetReceivedCommands<T>() where T : ICommand
+        {
+            lock (_receivedCommands)
+                return _receivedCommands.OfType<T>().ToList();
+        }
+
+        public void Dispose()
+        {
+            _isDisposing = true;
+            _client.Dispose();
+            // TODO - reenable once LibAtem allows disconnection
+            // Assert.True(_disposeEvent.WaitOne(TimeSpan.FromSeconds(1)), "LibAtem: Cleanup timed out");
+        }
+
+        public delegate void SwitcherEventHandler(object sender, object args);
+
+        private class SwitcherConnectionMonitor : IBMDSwitcherCallback
+        {
+            // Events:
+            public event SwitcherEventHandler SwitcherDisconnected;
+
+            void IBMDSwitcherCallback.Notify(_BMDSwitcherEventType eventType, _BMDSwitcherVideoMode coreVideoMode)
+            {
+                if (eventType == _BMDSwitcherEventType.bmdSwitcherEventTypeDisconnected)
+                {
+                    SwitcherDisconnected?.Invoke(this, null);
+                }
+            }
+        }
+    }
+}
---
+++ AtemEmulator.ComparisonTests/TestProductName.cs
@@ -0,0 +1,25 @@
+﻿using LibAtem.Commands.DeviceProfile;
+using Xunit;
+
+namespace AtemEmulator.ComparisonTests
+{
+    public class TestProductName : AtemCommandTestBase
+    {
+        protected override bool LogLibAtemHandshake => true;
+
+        [Fact]
+        public void Test1()
+        {
+            string sdkName;
+            _sdkSwitcher.GetProductName(out sdkName);
+            Assert.NotNull(sdkName);
+
+            var cmds = GetReceivedCommands<ProductIdentifierCommand>();
+            Assert.Equal(1, cmds.Count);
+
+            Assert.Equal(sdkName, cmds[0].Name);
+            Assert.InRange(sdkName.Length, 5, 40);
+        }
+    }
+}
+
