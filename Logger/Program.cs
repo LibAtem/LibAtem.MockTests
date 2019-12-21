094c7ad7a3edabd418ff871f59c46074f3cf9261
Julian Waller <git@julusian.co.uk>
2019-12-21 15:47:09 +0000

    feat: add minimal logger app

---
+++ Logger/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibAtem.Net;
+using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            var log = LogManager.GetLogger(typeof(Program));
+            log.Info("Starting");
+
+            var client = new AtemClient("10.42.13.99");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
